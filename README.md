98e0908a53289473f2ba2af91387b5a564b5b150
Matrej <blaha.matej@gmail.com>
2023-02-18 10:49:28 +0100

    Add first 'non-functional' prototype. Enable click on number buttons to write first variable for computation.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MyAwesomeCalculator
 Javascript project with functional simple calculator
+
+I Introduce you my 'awesome' and yet simple calculator which cannot calculate anything YET!
+You can only write your first number for computaion.
+Next features to do:
+                        - make listener for operators clicks and display them on the calculator's display
+                        - enable write the seccond number for computation
+                        - finally - calculate the result!
+                        - enable reset and delete buttons
+                        - show history of calculated results.
--- calculator.js
+++ calculator.js
@@ -1,11 +1,73 @@
-const field = document.querySelector('.inputField');
+
+let firstNumber = null;
+let firstOperator = null;
+let seccondNumber = null;
+let seccondOperator = null;
+let displayValue = 0;
+
+const display = document.querySelector('.display');
+const buttons = Array.from(document.querySelectorAll('button'));
+
+display.textContent = displayValue;
+window.addEventListener('keydown', e => {
+            let button = document.querySelector(`button[data-key="${e.keyCode}"]`)
+            button.classList.add('active');
+        }
+);
+buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('active');
+  }
+
 const numbers = document.querySelectorAll('.number');
+numbers.forEach(number => {
+    number.addEventListener('click', e => {
+        if (seccondNumber === null && firstOperator === null) {
+            updateFirstNumber(e.target.textContent);
+        } else if (firstOperator !== null) {
+            updateSeccondNumber(e.target.textContent);
+        }
+        updateDisplay();
+        return;
+    })
+});
 
-
-numbers.forEach((number) => {
-    number.addEventListener('click', e => enterNumber(e));
-})
-
-function enterNumber (num) {
-    field.textContent += num.target.textContent;
+function updateFirstNumber (withNumber) {
+    if (firstNumber === null) {
+        firstNumber  = withNumber;
+    } else {
+        firstNumber += withNumber;
+    }
+};
+function updateSeccondNumber (withNumber) {
+    if (seccondNumber === null) {
+        seccondNumber  = withNumber;
+    } else {
+        seccondNumber += withNumber;
+    }
+};
+function updateDisplay () {
+    if (seccondNumber === null && firstOperator === null) {
+        displayValue = `${firstNumber}`;
+    } else if (seccondNumber === null) {
+        displayValue = `${firstNumber} ${firstOperator}`
+    } else {
+        displayValue = `${firstNumber} ${firstOperator} ${seccondNumber}`;
+    }
+    display.textContent = displayValue;
 }
+
+function addiction (num1, num2) {
+    field.textContent = num1 + num2;
+};
+function substraction (num1, num2) {
+    field.textContent = num1 - num2;
+};
+function multiplication (num1, num2) {
+    field.textContent = num1 * num2;
+};
+function division (num1, num2) {
+    field.textContent = num1 / num2;
+};
